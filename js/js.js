3c98888ddbd61958cd7efcf5fce3d7bbe63aed63
Janprakash Joshi <88995621+Janprakash-Joshi@users.noreply.github.com>
2022-06-13 03:11:11 +0545

    Add files via upload

--- js/js.js
+++ js/js.js
@@ -8,11 +8,21 @@ setTimeout(abc,5000)
        var b = document.getElementById('home');
        var c = document.getElementById('contact');
        var d = document.getElementById('blogs'); 
-           
+
+       var e=document.getElementById('ind1');  
+       var f=document.getElementById('ind2');
+       var g=document.getElementById('ind3');
+       var h=document.getElementById('ind4');  
+
           a.style.display = 'block';
           b.style.display = 'none';
           c.style.display = 'none';
           d.style.display = 'none';
+
+          e.style.display = 'none';
+          f.style.display = 'none';
+          g.style.display = 'block';
+          h.style.display = 'none';
    }
 
    function home() {
@@ -20,11 +30,21 @@ setTimeout(abc,5000)
        var b = document.getElementById('home');
        var c = document.getElementById('contact');
        var d = document.getElementById('blogs'); 
+
+       var e=document.getElementById('ind1');  
+       var f=document.getElementById('ind2');
+       var g=document.getElementById('ind3');
+       var h=document.getElementById('ind4');
            
           a.style.display = 'none';
           b.style.display = 'block';
-          c.style.display = 'none';7
+          c.style.display = 'none';
           d.style.display = 'none';
+
+          e.style.display = 'block';
+          f.style.display = 'none';
+          g.style.display = 'none';
+          h.style.display = 'none';
    }
 
    function contact() {
@@ -32,11 +52,21 @@ setTimeout(abc,5000)
        var b = document.getElementById('home');
        var c = document.getElementById('contact');
        var d = document.getElementById('blogs'); 
-           
+
+       var e=document.getElementById('ind1');  
+       var f=document.getElementById('ind2');
+       var g=document.getElementById('ind3');
+       var h=document.getElementById('ind4');
+
           a.style.display = 'none';
           b.style.display = 'none';
           c.style.display = 'block';
           d.style.display = 'none';
+
+          e.style.display = 'none';
+          f.style.display = 'block';
+          g.style.display = 'none';
+          h.style.display = 'none';
    }
 
     function blogs() {
@@ -45,8 +75,18 @@ setTimeout(abc,5000)
        var c = document.getElementById('contact');
        var d = document.getElementById('blogs'); 
 
+       var e=document.getElementById('ind1');  
+       var f=document.getElementById('ind2');
+       var g=document.getElementById('ind3');
+       var h=document.getElementById('ind4');
+
           a.style.display = 'none';
           b.style.display = 'none';
           c.style.display = 'none';
           d.style.display = 'block';
+
+          e.style.display = 'none';
+          f.style.display = 'none';
+          g.style.display = 'none';
+          h.style.display = 'block';
    }
