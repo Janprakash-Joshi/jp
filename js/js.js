ee0fa8ad3dd008badbf198ba7d25109c31c48ac8
Janprakash Joshi <88995621+Janprakash-Joshi@users.noreply.github.com>
2022-06-18 14:40:01 +0545

    Add files via upload

--- js/js.js
+++ js/js.js
@@ -45,6 +45,8 @@ setTimeout(abc,5000)
           f.style.display = 'none';
           g.style.display = 'none';
           h.style.display = 'none';
+
+          
    }
 
    function contact() {
@@ -90,3 +92,51 @@ setTimeout(abc,5000)
           g.style.display = 'none';
           h.style.display = 'block';
    }
+
+
+   const scriptURL = 'https://script.google.com/macros/s/AKfycbwnSxUOXkD7a_janabPi9oxfHohfmPYUUKdHszAW65wlLuy_UcPL6t4FPg4yewDMQfwxw/exec'
+  const form = document.forms['submit-to-google-sheet']
+  const form1 = document.forms['submit-to-google-sheet1']
+  const form2 = document.forms['submit-to-google-sheet2']
+
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => alert("Thank You. We have received your message."))
+      .catch(error => console.error('Error!', error.message))
+      var frm = document.getElementsByName('submit-to-google-sheet')[0];
+   // frm.submit(); // Submit
+   frm.reset();
+  })
+
+  form1.addEventListener('submit', f => {
+   f.preventDefault()
+   fetch(scriptURL, { method: 'POST', body: new FormData(form1)})
+     .then(response => alert("Thank You. Greeting from Janprakash Joshi"))
+     .catch(error => console.error('Error!', error.message))
+     var frm = document.getElementsByName('submit-to-google-sheet1')[0];
+  // frm.submit(); // Submit
+  frm.reset();
+ })
+  
+ form2.addEventListener('submit', g => {
+   g.preventDefault()
+   fetch(scriptURL, { method: 'POST', body: new FormData(form2)})
+     .then(response => alert("Thank You. We have received your message."))
+     .catch(error => console.error('Error!', error.message))
+     var frm = document.getElementsByName('submit-to-google-sheet2')[0];
+  // frm.submit(); // Submit
+  frm.reset();
+ })
+  
+
+  
+
+//   function submitForm() {
+//    // Get the first form with the name
+//    // Hopefully there is only one, but there are more, select the correct index
+//    var frm = document.getElementsByName('submit-to-google-sheet')[0];
+//    // frm.submit(); // Submit
+//    frm.reset();  // Reset
+//    return false; // Prevent page refresh
+// }
